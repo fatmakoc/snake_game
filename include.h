938940a898bba915c35c2cf3528aaba05dba737e
Fatma Koç <fatmakoc94@gmail.com>
2014-06-23 16:06:50 +0300

    Create include.h

---
+++ include.h
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "iostream"
+#include  <clocale>
+#include <ctime>
+# include <stdio.h> 
+#include <Windows.h>
+# include <conio.h> 
+# include <dos.h> 
+# include <string.h> 
+# include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <cstdlib>
+
+using namespace std;
